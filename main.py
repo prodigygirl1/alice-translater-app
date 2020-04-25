ecae428340207566383f4787aa2d887bc3e1f07f
prodigygirl1 <Prodigygirl1.0@yandex.ru>
2020-04-25 21:59:43 +0400

    add apikey

--- main.py
+++ main.py
@@ -57,10 +57,12 @@ def get_translate(word):
     response = requests.get(url, params=params)
     if response:
         js_resp = response.json()
+        print('ПЕРЕВОД:', js_resp['text'])
         return js_resp['text']
     return None
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
